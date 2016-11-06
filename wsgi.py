ac4d6123a59821b484f28f4667e5493ff36e2aa9
amazingguni <amazingguni@gmail.com>
2016-11-06 17:21:20 +0900

    Allow only GET method to access root page

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,11 @@ from flask import send_from_directory
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return send_from_directory('client', 'index.html')
 
-
 if __name__ == '__main__':
     app.run()
+
+    
