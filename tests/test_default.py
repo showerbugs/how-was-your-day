6fbf6be0292b52c05fb7330a27f60d483e9d02e8
amazingguni <amazingguni@gmail.com>
2016-11-08 22:26:51 +0900

    Remove unused __main__ statement

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,6 +4,3 @@ import unittest
 class DefaultTestCase(unittest.TestCase):
     def testTrue(self):
         self.assertTrue('This testcase should be success.', True)
-
-if __name__ == '__main__':
-    unittest.main()
