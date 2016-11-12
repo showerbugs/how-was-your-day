5b69285ec827603387a5c256815c6323b3a89f5b
qodot <waitingforqodot@gmail.com>
2016-11-12 22:12:17 +0900

    Impl local_config command
    get configs with user prompt,
    read config/local.py.tmpl, write local.py

--- commands/local_config.py
+++ commands/local_config.py
@@ -1,6 +1,23 @@
-import click
+from click import command
+from click import prompt
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
 
 
-@click.command()
+@command()
 def local_config():
-    print('------------')
+    user = prompt('Enter your DB user', type=str)
+    password = prompt('Enter your DB user\'s password '
+                      '(You can pass this step, just push enter)',
+                      type=str, default='', show_default=False)
+    dbname = prompt('Enter your DB name', type=str, default='howwasyourday')
+    dbport = prompt('Enter your DB port', type=int, default=5432)
+    test_dbname = prompt('Enter your TEST DB name',
+                         type=str, default='howwasyourday_test')
+
+    env = Environment(loader=FileSystemLoader('config'))
+    tmpl = env.get_template('local.py.tmpl')
+    localpy = tmpl.render(user=user, password=password,
+                          dbname=dbname, dbport=dbport, test_dbname=test_dbname)
+    with open('config/local.py', 'w') as fp:
+        fp.write(localpy)
