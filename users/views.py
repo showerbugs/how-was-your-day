0c227279ed0b334ef8df17a754e44636d2aa6994
amazingguni <amazingguni@gmail.com>
2016-11-23 23:33:52 +0900

    Add signup api feature
    - Adding [POST] /users TCs
    - Adding constructor, repr in User model

--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
 from flask import Blueprint
+from flask import request
 from flask import jsonify
+from db.session import Session
+from db.models import User
 
 app = Blueprint('users', __name__)
+session = Session()
 
-
-@app.route('/signin', methods=['POST'])
+@app.route('', methods=['POST'])
 def signin():
+    email = request.form['email']
+    name = request.form['name']
+    password = request.form['password']
+    
+    session.add(User(email, password, name))
+    session.commit()
     return jsonify(result={'success': True})
