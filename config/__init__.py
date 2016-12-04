69310fa91039746b08503a42443f1396ca5919c3
amazingguni <amazingguni@gmail.com>
2016-12-04 21:14:45 +0900

    Add travis database configure

--- config/__init__.py
+++ config/__init__.py
@@ -14,6 +14,7 @@ def get_config():
         'local': LocalConfig,
         'dev': DevConfig,
         'prod': ProdConfig,
+        'travis'
     }[env]
 
     config = {}
---
+++ config/travis.py
@@ -0,0 +1,18 @@
+from config.base import BaseConfig
+
+
+class TravisConfig(BaseConfig):
+    DEBUG = True
+
+    DB_USER = 'postgres'
+    DB_PASSWORD = ''
+    DB_HOST = 'localhost'
+    DB_PORT = 5432
+    DB_NAME = 'howwasyourday'
+    DB_URL = 'postgresql://{}:{}@{}:{}/{}'.format(
+        DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+    TEST_DB_NAME = 'howwasyourday_test'
+    TEST_DB_URL = 'postgresql://{}:{}@{}:{}/{}'.format(
+        DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, TEST_DB_NAME)
+    import os
+    ALEMBIC_INI = os.path.join(os.getcwd(),'alembic.ini')
