134cf6855c90c531c91bf957ae0ef40023aa77db
qodot <waitingforqodot@gmail.com>
2016-12-22 22:20:52 +0900

    Add signin API

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 from flask import request
 from flask import jsonify
 from flask import g
+from flask_login import login_user
+
 from db.models import User
 
 app = Blueprint('users', __name__)
@@ -9,8 +11,16 @@ app = Blueprint('users', __name__)
 
 @app.route('/signin', methods=['POST'])
 def signin():
-    email = request.form['email']
-    name = request.form['name']
-    password = request.form['password']
-    g.db.add(User(email, password, name))
+    params = request.json
+    email = params['email']
+    password = params['password']
+
+    user = g.db.query(User)\
+        .filter(User.email == email,
+                User.password == password)\
+        .first()
+    if not user:
+        return jsonify(result={'success': False})
+
+    login_user(user)
     return jsonify(result={'success': True})
