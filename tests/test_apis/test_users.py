4eae84aaf469fe648eaede277afa06dc9edddc1a
amazingguni <amazingguni@gmail.com>
2016-12-26 20:25:45 +0900

    Add testcase when password and repeat is different

--- tests/test_apis/test_users.py
+++ tests/test_apis/test_users.py
@@ -61,3 +61,21 @@ class TestUser:
         new_user = session.query(User) \
             .filter(User.email == 'new@email.com').first()
         assert new_user
+
+    def test_create_user_repeated_password_different(self, flask_client, session):
+        # Given password와 password_repeat가 다르게 주어지고
+        data = json.dumps({'email': 'new@email.com', 'name': 'new',
+                           'password': '123123', 'password_repeat': 'different'})
+
+        # When users로 post method를 호출하면
+        resp = flask_client.post('/users/', data=data,
+                                 content_type='application/json')
+
+        # Then 회원가입이 실패한다 응답이 돌아오고 DB에 저장되지 않는다.
+        result = json.loads(resp.data.decode())
+        assert resp.status_code == 200
+        assert result['success'] == False
+
+        user_list = session.query(User) \
+            .filter(User.email == 'new@email.com').all()
+        assert len(user_list) == 0
