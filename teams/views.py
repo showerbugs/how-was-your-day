c0dd90fe3b2cd01b836b442085bfc82fee97d873
qodot <waitingforqodot@gmail.com>
2017-01-04 17:37:32 +0900

    Fix create team API with additional params

--- teams/views.py
+++ teams/views.py
@@ -5,6 +5,7 @@ from flask import g
 from flask_login import current_user
 from flask_login import login_required
 
+from db.models import User
 from db.models import Team
 
 
@@ -23,7 +24,8 @@ def list_teams():
 @login_required
 def get_team(team_id):
     team = current_user.teams.filter(Team.id == team_id).first()
-    return jsonify(success=True, data={'team': team.to_dict()})
+    team = team.to_dict()
+    return jsonify(success=True, data={'team': team})
 
 
 @app.route('/', methods=['POST'])
@@ -31,7 +33,14 @@ def get_team(team_id):
 def create_team():
     params = request.json
     name = params['name']
-    new_team = Team(name=name)
-    new_team.users.append(current_user.unwrap)
+    description = params['description']
+    users = g.db.query(User)\
+        .filter(User.email.in_(params['userEmails']))
+    owner = current_user.unwrap
+
+    new_team = Team(name=name, description=description, owner_id=owner.id)
+    for user in users:
+        new_team.users.append(user)
     g.db.add(new_team)
-    return jsonify(success=True)
+
+    return jsonify(success=True, data={})
