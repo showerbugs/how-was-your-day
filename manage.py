2e9c031ac8739f6c4197d1c2dea0d5897301f88d
amazingguni <amazingguni@gmail.com>
2017-01-28 21:39:05 +0900

    Add command to migrate database with alembic

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 import click
 
+from commands.create_local_db import create_local_db
 from commands.drop_local_db import drop_local_db
-from commands.generate_dummy_data import generate_dummy_data
+from commands.generate_dummy import generate_dummy
 from commands.local_config import local_config
+from commands.migration import migration
 from commands.runserver import runserver
 
 
@@ -13,8 +15,10 @@ def cli():
 
 cli.add_command(local_config)
 cli.add_command(runserver)
-cli.add_command(generate_dummy_data)
+cli.add_command(migration)
+cli.add_command(generate_dummy)
 cli.add_command(drop_local_db)
+cli.add_command(create_local_db)
 
 if __name__ == '__main__':
     cli()
