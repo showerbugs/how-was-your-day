353fcfa217d9ed33ee5473d3af8e23b08d5bacfe
amazingguni <amazingguni@gmail.com>
2017-01-28 20:47:19 +0900

    Add command to drop local database

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 import click
 
+from commands.drop_local_db import drop_local_db
+from commands.generate_dummy_data import generate_dummy_data
 from commands.local_config import local_config
 from commands.runserver import runserver
-from commands.generate_dummy_data import generate_dummy_data
 
 
 @click.group()
@@ -13,6 +14,7 @@ def cli():
 cli.add_command(local_config)
 cli.add_command(runserver)
 cli.add_command(generate_dummy_data)
+cli.add_command(drop_local_db)
 
 if __name__ == '__main__':
     cli()
